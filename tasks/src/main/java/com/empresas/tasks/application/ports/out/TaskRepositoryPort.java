464de6513b49373c1bd07237e5fd3e6c5993e7f7
GabrielLopez <gmlopez11@espe.edu.ec>
2026-05-09 17:31:25 -0500

    cambio a nuevo sistema

--- tasks/src/main/java/com/empresas/tasks/application/ports/out/TaskRepositoryPort.java
+++
@@ -1,9 +0,0 @@
-package com.empresas.tasks.application.ports.out;
-
-import com.empresas.tasks.domain.model.Task;
-import java.util.Optional;
-
-public interface TaskRepositoryPort {
-    Task save(Task task);
-    Optional<Task> findById(Long id);
-}
--- tasks/src/main/java/com/empresas/tasks/domain/model/TaskStatus.java
+++
@@ -1,8 +0,0 @@
-package com.empresas.tasks.domain.model;
-
-public enum TaskStatus {
-    PENDING,
-    IN_PROGRESS,
-    COMPLETED,
-    CANCELLED
-}
